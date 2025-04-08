40e727d76aa40f6bd3db751bca0143992d925a77
IrynaVB2023 <irinakhiba@gmail.com>
2025-04-08 22:58:05 +0300

    add connection file

--- file/connection.php
+++ file/connection.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "16.171.151.38";
+$username = "bloodbank_user";
+$password = "bloodbank_password";
 $dbname = "bloodbank";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if(!$conn){
